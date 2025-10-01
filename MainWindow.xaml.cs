254e9d8ac2aaf9843cdabe50c99c6475cdf3d705
Noah Zandona <noah.zandona@omega2000.it>
2025-10-01 20:15:44 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+
+namespace HMI_template_v0._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Start: Button Close | Restore | Minimize 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        // End: Button Close | Restore | Minimize
+
+        // Questi sono gli unici eventi dei bottoni del menu che servono ancora,
+        // per la navigazione tra le pagine.
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Home.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void btnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Dashboard.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        // Aggiungi qui gli altri eventi 'Click' per la navigazione se necessario
+        // Esempio:
+        // private void btnProducts_Click(object sender, RoutedEventArgs e)
+        // {
+        //     fContainer.Navigate(new System.Uri("Pages/Products.xaml", UriKind.RelativeOrAbsolute));
+        // }
+    }
+}
